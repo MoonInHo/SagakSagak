5c61187d574a5d7ce2e2281a2abf9a1e16bcb62d
MoonInHo <ansdlsgh55@gmail.com>
2023-08-06 18:35:12 +0900

    first commit

---
+++ src/main/java/sagak/sagaksagak/SagakSagakApplication.java
@@ -0,0 +1,13 @@
+package sagak.sagaksagak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagakSagakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagakSagakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sagak/sagaksagak/SagakSagakApplicationTests.java
@@ -0,0 +1,13 @@
+package sagak.sagaksagak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagakSagakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
